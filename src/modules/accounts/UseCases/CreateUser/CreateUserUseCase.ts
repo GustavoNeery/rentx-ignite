0454d26e3a708d8c66ae77af791ffd89367ffa38
GustavoNeery <gustavoalvesneryy@hotmail.com>
2022-04-22 23:58:50 -0300

    Feature password com hash

--- src/modules/accounts/UseCases/CreateUser/CreateUserUseCase.ts
+++ src/modules/accounts/UseCases/CreateUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
+
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -10,13 +12,21 @@ class CreateUserUseCase {
         private usersRepository: IUsersRepository
     ) {}
 
-    async execute({name, username, email, password, driver_license}: ICreateUserDTO): Promise<void> {
+    async execute({name, email, password, driver_license}: ICreateUserDTO): Promise<void> {
+
+        cosnt userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const passwordHash = await hash(password, 8);
+
 
         await this.usersRepository.create({
             name,
-            username,
             email,
-            password,
+            password: passwordHash,
             driver_license
         });
     }
